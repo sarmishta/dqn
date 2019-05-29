ca20c9b35cb9e0005a3bc7accec70166e1c1b484
sarmishta <bsarmishta@gmail.com>
2019-05-29 11:06:15 -0700

    edits

--- dqn.py
+++ dqn.py
@@ -26,6 +26,8 @@ class QLearner(nn.Module):
             nn.Conv2d(32, 64, kernel_size=4, stride=2),
             nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
             nn.ReLU()
         )
         
@@ -51,37 +53,43 @@ class QLearner(nn.Module):
             # TODO: Given state, you should write code to get the Q value and chosen action
             # Complete the R.H.S. of the following 2 lines and uncomment them
             q_value =self.forward(state)
-            action =q_value.max(1)[1].data[0]
+            action = q_value.max(1)[1].data[0]
             ######## YOUR CODE HERE! ########
-    
         else:
             action = random.randrange(self.env.action_space.n)
         return action
         
 def compute_td_loss(model, batch_size, gamma, replay_buffer):
-    state, action, reward, next_state, done = replay_buffer.sample(batch_size)
+    states, actions, rewards, next_states, dones = replay_buffer.sample(batch_size)
 
-    state = Variable(torch.FloatTensor(np.float32(state)))
-    next_state = Variable(torch.FloatTensor(np.float32(next_state)), requires_grad=True)
-    action = Variable(torch.LongTensor(action))
-    reward = Variable(torch.FloatTensor(reward))
-    done = Variable(torch.FloatTensor(done))
+    states = Variable(torch.FloatTensor(np.float32(states)))
+    next_states = Variable(torch.FloatTensor(np.float32(next_states)), requires_grad=True)
+    actions = Variable(torch.LongTensor(actions))
+    rewards = Variable(torch.FloatTensor(rewards))
+    done = Variable(torch.FloatTensor(dones))
 
     ######## YOUR CODE HERE! ########
     # TODO: Implement the Temporal Difference Loss
-    # loss =
-    ######## YOUR CODE HERE! ########
-    q_values      = model(state)
-    next_q_values = model(next_state)
+    q_values = model(states)
+    next_q_values = model(next_states)
 
-    q_value          = q_values.gather(1, action.unsqueeze(1)).squeeze(1)
-    next_q_value     = next_q_values.max(1)[0]
-    expected_q_value = reward + gamma * next_q_value * (1 - done)
-    
+    q_value = q_values.gather(1, actions.unsqueeze(1)).squeeze(1)
+    next_q_value = next_q_values.max(1)[0]
+    expected_q_value = rewards + gamma * next_q_value * (1-done)
     loss = (q_value - Variable(expected_q_value.data)).pow(2).mean()
-
-
+    ######## YOUR CODE HERE! ########
     return loss
+
+def plot(frame_idx, rewards, losses, plot_dir):
+    plt.figure(figsize=(20,4))
+    plt.subplot(131)
+    plt.title('frame %s. reward: %s' % (frame_idx, np.mean(rewards[-10:])))
+    plt.plot(rewards)
+    plt.subplot(132)
+    plt.title('loss')
+    plt.plot(losses)
+    plt.savefig(plot_dir+'/%s-%s.png' % (frame_idx, np.mean(rewards[-10:])))
+    plt.close()
 
 
 class ReplayBuffer(object):
@@ -94,14 +102,18 @@ class ReplayBuffer(object):
 
         self.buffer.append((state, action, reward, next_state, done))
 
+
     def sample(self, batch_size):
         ######## YOUR CODE HERE! ########
         # TODO: Randomly sampling data with specific batch size from the buffer
         # Hint: you may use the python library "random".
         # If you are not familiar with the "deque" python library, please google it.
+        # state, action, reward, next_state, done = zip(*random.sample(self.buffer, batch_size))
+        indices = np.random.choice(len(self.buffer)-1, batch_size, replace=False)
+        states, actions, rewards, next_states, dones = zip(*[self.buffer[idx] for idx in indices])
         ######## YOUR CODE HERE! ########
-        state, action, reward, next_state, done = zip(*random.sample(self.buffer, batch_size))
-        return np.concatenate(state), action, reward, np.concatenate(next_state), done
+
+        return np.concatenate(states), actions, rewards, np.concatenate(next_states), dones
 
     def __len__(self):
         return len(self.buffer)
--- run_dqn_pong.py
+++ run_dqn_pong.py
@@ -1,45 +1,62 @@
+import os
 from Wrapper.layers import *
 from Wrapper.wrappers import make_atari, wrap_deepmind, wrap_pytorch
-import math, random
-import gym
+import math
 import numpy as np
-import matplotlib.pyplot as plt
 import torch
-import torch.nn as nn
 import torch.optim as optim
-import torch.autograd as autograd 
-import torch.nn.functional as F
 USE_CUDA = torch.cuda.is_available()
-from dqn import QLearner, compute_td_loss, ReplayBuffer
+from dqn import QLearner, compute_td_loss, ReplayBuffer, plot
 
-env_id = "PongNoFrameskip-v4"
-env = make_atari(env_id)
-env = wrap_deepmind(env)
-env = wrap_pytorch(env)
 
-num_frames = 1000000
-batch_size = 32
-gamma = 0.99
-    
+num_frames = 1600000
+batch_size = 64
+gamma = 0.97  # The discount factor 
 replay_initial = 10000
-replay_buffer = ReplayBuffer(100000)
-model = QLearner(env, num_frames, batch_size, gamma, replay_buffer)
-optimizer = optim.Adam(model.parameters(), lr=0.00001)
-if USE_CUDA:
-    model = model.cuda()
-
+replay_buffer_max = 100000
+lr = 0.00001
 epsilon_start = 1.0
 epsilon_final = 0.01
 epsilon_decay = 30000
+
+cwd = os.getcwd()
+dir_name = str(num_frames)+"-"+str(replay_initial)
+if not os.path.exists(dir_name):
+    os.mkdir(dir_name)
+
+plot_dir = dir_name+"/plots"
+if not os.path.exists(plot_dir):
+    os.mkdir(plot_dir)
+
+video_dir = dir_name+"/video"
+if not os.path.exists(video_dir):
+    os.mkdir(video_dir)
+
+#log file
+file_w = "dump_log.csv"
+f = open(dir_name+"/"+file_w, 'w')
+f.write('Frames,Losses,Mean Rewards+\n')
+
+env_id = "PongNoFrameskip-v4"
+env, env_to_wrap = make_atari(env_id, video_dir)
+env = wrap_deepmind(env)
+env = wrap_pytorch(env)
+
+replay_buffer = ReplayBuffer(replay_buffer_max)
+model = QLearner(env, num_frames, batch_size, gamma, replay_buffer)
+optimizer = optim.Adam(model.parameters(), lr=lr)
+if USE_CUDA:
+    model = model.cuda()
+
+
 epsilon_by_frame = lambda frame_idx: epsilon_final + (epsilon_start - epsilon_final) * math.exp(-1. * frame_idx / epsilon_decay)
 
 losses = []
 all_rewards = []
 episode_reward = 0
 
+
 state = env.reset()
-
-
 for frame_idx in range(1, num_frames + 1):
 
     epsilon = epsilon_by_frame(frame_idx)
@@ -69,3 +86,10 @@ for frame_idx in range(1, num_frames + 1):
     if frame_idx % 10000 == 0 and len(replay_buffer) > replay_initial:
         print('#Frame: %d, Loss: %f' % (frame_idx, np.mean(losses)))
         print('Last-10 average reward: %f' % np.mean(all_rewards[-10:]))
+
+        f.write('%d,%f,%f' % (frame_idx, np.mean(losses), np.mean(all_rewards[-10:]))+"\n")
+        plot(frame_idx, all_rewards, losses, plot_dir)
+
+f.close()
+env.close()
+env_to_wrap.close()
